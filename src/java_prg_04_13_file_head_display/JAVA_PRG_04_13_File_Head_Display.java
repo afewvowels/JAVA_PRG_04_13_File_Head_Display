7adc0f4a79605579746a506ce118129f2f6b9b08
Keith Smith <32456218+afewvowels@users.noreply.github.com>
2017-10-29 12:41:27 -0400

    Prompt written.

---
+++ src/java_prg_04_13_file_head_display/JAVA_PRG_04_13_File_Head_Display.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package java_prg_04_13_file_head_display;
+
+/**
+ *
+ * @author bluebackdev
+ * Write a program that asks the user for the name of a file. The program should
+ * display only the first five lines of the file's contents. If the file contains
+ * fewer than five lines, it should display the file's entire contents.
+ */
+public class JAVA_PRG_04_13_File_Head_Display {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
